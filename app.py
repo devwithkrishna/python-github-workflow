694e0b090eb9e8f2bbb8141c6023054da67e7645
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2023-12-06 19:01:52 +0530

    added one more job in workflow DEVOPS-13

---
+++ app.py
@@ -0,0 +1,22 @@
+# app.py
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+# Configuration for SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+
+@app.route('/')
+def home():
+    return 'Hello, Flask Web App!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
